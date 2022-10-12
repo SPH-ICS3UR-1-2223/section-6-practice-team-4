dafb00dd0ce2f80ccf7d66d3d5ee41ccc090baad
aminstu <amin.ben-errabeh@stu.ocsb.ca>
2022-10-12 13:49:59 -0400

    ValidatePin Finished

--- src/ValidatePin/ValidatePin.java
+++ src/ValidatePin/ValidatePin.java
@@ -5,6 +5,15 @@ import java.util.Scanner;
 public class ValidatePin {
 
     public static void main(String[] args) {
-        
+    	Scanner in =  new Scanner(System.in);
+    	int secretNumber = 13;
+    	System.out.println("INSERT PIN");
+    	int guess =  in.nextInt();
+    	while (guess!=secretNumber) {
+    		System.out.println("WRONG");
+    		guess = in.nextInt();
+    	}
+    	
+    	System.out.println("CORRECT");
     }
 }
