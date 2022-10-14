71b43437fa0ec518c35b8708e99933825a0eae89
aminstu <amin.ben-errabeh@stu.ocsb.ca>
2022-10-14 14:33:49 -0400

    finished everything

--- src/DisplayMultiples/DisplayMultiples.java
+++ src/DisplayMultiples/DisplayMultiples.java
@@ -1,11 +1,28 @@
 package DisplayMultiples;
-
 import java.util.Scanner;
 
 public class DisplayMultiples {
 
     public static void main(String args[]) {
-       
+    	Scanner in =  new Scanner(System.in);
+    	System.out.println("Guess the secret number");
+    	int number = in.nextInt();
+    	for (int counter=1;counter<=12;counter++) {
+    		System.out.println(number+" * "+counter+" = "+(number*counter));
+    	}
+//    System.out.println(number + "* 1");
+//    System.out.println(number + "* 2");
+//    System.out.println(number);
+//    System.out.println(number);
+//    System.out.println();
+//    System.out.println();
+//    System.out.println();
+//    System.out.println();
+//    System.out.println();
+//    System.out.println();
+//    System.out.println();
+//    System.out.println();
+//    System.out.println();
     }
 
 }
