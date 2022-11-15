c5be444ecb29ca4bbf23230728ab19068f5492c5
aminstu <amin.ben-errabeh@stu.ocsb.ca>
2022-11-15 13:59:13 -0500

    finsih

--- src/DisplayMultiples/DisplayMultiples.java
+++ src/DisplayMultiples/DisplayMultiples.java
@@ -5,7 +5,7 @@ public class DisplayMultiples {
 
     public static void main(String args[]) {
     	Scanner in =  new Scanner(System.in);
-    	System.out.println("Guess the secret number");
+    	System.out.println("say a number!");
     	int number = in.nextInt();
     	for (int counter=1;counter<=12;counter++) {
     		System.out.println(number+" * "+counter+" = "+(number*counter));
